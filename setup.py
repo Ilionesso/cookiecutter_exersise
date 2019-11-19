a07c196040420be27db525729ef66829dd6f1488
Ilionesso <ilionesso@gmail.com>
2019-11-19 19:29:54 +0100

    v.ololo?8

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ilionesso/cookiecutter_exersise',
-    version='0.1.0',
+    version='0.2.5',
     zip_safe=False,
 )
